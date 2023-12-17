d6f5daf2e564df367e9dff123595956d7e0391a2
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-17 16:51:09 +0500

    Automatically close other "secondchild" divs when one is opened

--- js/index.js
+++ js/index.js
@@ -1,35 +1,43 @@
-// Logic for website's 6th Section 
+// Logic for website's 6th Section
+
+function toggleVisibility(clickedElement) {  
+
+  // Get the parent div of the clicked child
+  let parentDiv = clickedElement.parentElement;
+
+  // Find the second child div within the parent div
+  let childTwo = parentDiv.querySelector(".secondchild");
+
+  // Toggle the visibility of the clicked "secondchild" div
+  childTwo.classList.toggle("hidden");
+
+  // Toggle the symbol from + to multiply and vice versa
+  let symbol_element = clickedElement.querySelector(".symbol_element");
+  symbol_element.textContent = symbol_element.textContent === "+" ? "×" : "+";
 
 
-    function toggleVisibility(clickedElement) {
+    // Iterate over all FAQ items to close other "secondchild" divs
+    let allParents = document.querySelectorAll(".parent");
 
-        console.log(clickedElement);
-      // Get the parent div of the clicked child
-      var parentDiv = clickedElement.parentElement;
+    allParents.forEach((parent) => {
 
-      // Find the second child div within the parent div
-      var childTwo = parentDiv.querySelector('.secondchild');
-
-      // Toggle its visibility
-      childTwo.classList.toggle('hidden');
-
-    //   Also changing symbol from + to multiply 
-    var symbol_element = clickedElement.querySelector('.symbol_element');
-
-    console.log(symbol_element);
-
-    if (symbol_element.textContent === '+') {
-        symbol_element.textContent = '× ';
-    } 
-    else
-     {
-        symbol_element.textContent = '+'
-    }
+    let otherChild = parent.querySelector(".secondchild");
     
+    //Only left one opened in which user clicked and else close all other child divs
+    if (otherChild !== childTwo) {
+      otherChild.classList.add("hidden");
+      // Reset symbol to "+"
+      let otherSymbol = parent.querySelector(".symbol_element");
+      if (otherSymbol.textContent !== "+") {
+        otherSymbol.textContent = "+";
+      }
     }
 
+  });
+}
 
- /* Whats happening in above code: 
+
+/* For understanding above code: 
     
     <div class="parent">
       <div class="child" onclick="toggleVisibility(this)">  Heading  </div>
